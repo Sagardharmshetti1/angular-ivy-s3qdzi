44368ef447f33759d10156d930de49c9a779f0ec
Sagardharmshetti1 <114577861+Sagardharmshetti1@users.noreply.github.com>
2022-09-28 23:03:40 +0530

    3 files added

---
+++ src/app/model/user.ts
@@ -0,0 +1,12 @@
+
+export class User {
+    FName!: string;
+    LName!: string;
+    email!: string;
+    phoneNumber!: string;
+    company!: string;
+    gender!: string;
+    DOB!: string;
+    password!: string;
+    id!: string;
+}
